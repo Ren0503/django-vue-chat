a423e12474f778554c181f4c3b5bd24c4c620a37
Ren0503 <illuminati0440@gmail.com>
2022-01-22 15:20:33 +0700

    Add style

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import store from './store'
 import apolloProvider from './utils/apollo-client'
 import Vue from 'vue'
 import App from './App.vue'
+import "./styles/style.css"
 
 Vue.config.productionTip = DEBUG
 
