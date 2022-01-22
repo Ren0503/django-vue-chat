827f6b683e50a89a08f0f4465d2c21a1210ad8d3
Ren0503 <illuminati0440@gmail.com>
2022-01-22 10:48:36 +0700

    complete client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link :to="{ name: 'Login' }">Login</router-link> |
+      <router-link :to="{ name: 'Register' }">Register</router-link> |
+      <a href="#" @click="$store.commit('logout')">logout</a>
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  created() {
+    this.$store.commit("checkAuth");
+  },
+  computed: {
+    ...mapGetters(["token"]),
+  },
+};
 </script>
 
-<style>
+<style scope>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+  text-align: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,15 @@
+import { DEBUG } from 'utils/settings'
+import router from 'router'
+import store from 'store'
+import apolloProvider from 'utils/apollo-client'
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = DEBUG
 
 new Vue({
+  router,
+  apolloProvider,
+  store,
   render: h => h(App),
 }).$mount('#app')
